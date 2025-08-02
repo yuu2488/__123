7697ea3808e8b9d415dbe4147583f5e186da3b11
yuu2488 <green652799@gmail.com>
2025-08-02 09:25:33 +0100

    123

---
+++ lesson6/lesson6_5.py
@@ -0,0 +1,9 @@
+from google import genai
+
+# The client gets the API key from the environment variable `GEMINI_API_KEY`.
+client = genai.Client()
+
+response = client.models.generate_content(
+    model="gemini-2.5-flash", contents="天空為什麼是藍的?"
+)
+print(response.text)
